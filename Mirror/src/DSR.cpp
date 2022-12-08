802a42bc4c4fdc573d2e3908072f2bfd6b67f4dc
pawREP <pawREP@users.noreply.github.com>
2022-12-08 02:41:16 +0100

    Comment clean up

--- Mirror/src/DSR.cpp
+++ Mirror/src/DSR.cpp
@@ -2,19 +2,6 @@
 #include "DInputUtil.h"
 #include "XInputUtil.h"
 
-// namespace DSR {
-//
-//     intptr_t XInputGetStateOffset = 0x20B5CA0;
-//
-//     void patch() {
-//     }
-//
-//     void transformInput(XINPUT_STATE* state) {
-//         XInputUtil::mirrorLeftStickX(state);
-//     }
-//
-// } // namespace DSR
-
 void DSR::map(_XINPUT_STATE* pState) const {
     XInputUtil::mirrorLeftStickX(pState);
 }
