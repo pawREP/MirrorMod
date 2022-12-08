5988490b8c530d922b7073a8044b299d26f89184
pawREP <pawREP@users.noreply.github.com>
2022-12-08 02:40:23 +0100

    Update 'Game' type definition
    Game used to an alias for a namespace. We transitioned to using classes for all game, hence this change.

--- Mirror/src/Game.h
+++ Mirror/src/Game.h
@@ -2,14 +2,14 @@
 
 #if defined(BUILD_DS3)
 #include "DS3.h"
-namespace Game = DS3;
+using Game = DS3;
 #elif defined(BUILD_ELDENRING)
 #include "EldenRing.h"
 using Game = EldenRing;
 #elif defined(BUILD_DS2SOTFS)
 #include "DS2Sotfs.h"
-namespace Game = DS2Sotfs;
+using Game = DS2Sotfs;
 #elif defined(BUILD_DSR)
 #include "DSR.h"
-namespace Game = DSR;
+using Game = DSR;
 #endif
