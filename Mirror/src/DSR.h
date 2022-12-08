2f0f1ba57cde6da3d53910532f1555657c63d82d
pawREP <pawREP@users.noreply.github.com>
2022-12-08 02:59:06 +0100

    Cleanup ctor signature

--- Mirror/src/DSR.h
+++ Mirror/src/DSR.h
@@ -6,19 +6,9 @@
 
 class DSR : public IXInputMap, public IDInputMap {
 public:
-    explicit DSR([[maybe_unused]] const B3L::ImageView& image, [[maybe_unused]] const Configuration& config){
-
-    };
+    DSR([[maybe_unused]] const B3L::ImageView& image, [[maybe_unused]] const Configuration& config){};
 
     void map(_XINPUT_STATE* pState) const override;
     void map(_DIMOUSESTATE2* state) const override;
     void map(char* keyboardState) const override;
 };
-
-
-// namespace DSR {
-//     extern intptr_t XInputGetStateOffset;
-//
-//     void patch();
-//     void transformInput(XINPUT_STATE* state);
-// }; // namespace DSR
