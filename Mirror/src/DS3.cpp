f76ff7d01b866a50581f19f1cf16a73704f511db
pawREP <pawREP@users.noreply.github.com>
2022-12-10 15:44:28 +0100

    Add UI element screen space transform patch

--- Mirror/src/DS3.cpp
+++ Mirror/src/DS3.cpp
@@ -9,6 +9,13 @@
 DS3::DS3(const B3L::ImageView& image, const Configuration& config) {
     const auto desc = config["FXAASetting"].get<DeepPointerDescriptor>();
     fxaaSetting     = fromPointerDesc<int>(image, desc);
+
+    // Modify float used in routine that positions moveable ui elements. This fixes the screen space coordinates of lock-on dot and enemyHP bars on the mirrored render target.
+    const auto needle  = B3L::AOBPattern::fromString("00 00 00 3F 00 00 00 BF 00 00 00 3F 00 00 00 00").value();
+    const auto section = image.sectionData(2); //.rdata
+    auto it            = B3L::AOBScanner::find(section.begin(), section.end(), needle);
+    if(it != section.end())
+        reinterpret_cast<float&>(const_cast<uint8_t&>(*it)) = -0.5;
 }
 
 void DS3::map(_XINPUT_STATE* state) const {
