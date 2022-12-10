98d2bb9840bf1148df3a70c1f6247c075579f728
pawREP <pawREP@users.noreply.github.com>
2022-12-10 12:03:25 +0100

    Add UI alignment patch to Sekiro class

--- Mirror/src/Sekiro.cpp
+++ Mirror/src/Sekiro.cpp
@@ -1,6 +1,14 @@
 #include "Sekiro.h"
 #include "DInputUtil.h"
+#include "InlinePatchDeserializer.h"
 #include "XInputUtil.h"
+
+Sekiro::Sekiro(const B3L::ImageView& image, const Configuration& config) {
+    const auto uiPatchDesc = config["PatchUI"].get<PatchDescriptor>();
+
+    uiPatch = fromPatchDesc<B3L::InlinePatch>(image, uiPatchDesc);
+    uiPatch->enable();
+}
 
 void Sekiro::map(_XINPUT_STATE* pState) const {
     XInputUtil::mirrorLeftStickX(pState);
--- Mirror/src/Sekiro.h
+++ Mirror/src/Sekiro.h
@@ -1,20 +1,18 @@
 #pragma once
-#include "B3L/PrologHook.h"
+#include "B3L/InlineCallbackHook.h"
 #include "Configuration.h"
 #include "DInputHook.h"
 #include "XInputHook.h"
 #include <Windows.h>
 
-
-
-
-
 class Sekiro : public IXInputMap, public IDInputMap {
 public:
-    Sekiro([[maybe_unused]] const B3L::ImageView& image, [[maybe_unused]] const Configuration& config) {
-    };
+    Sekiro([[maybe_unused]] const B3L::ImageView& image, const Configuration& config);
 
     void map(_XINPUT_STATE* pState) const override;
     void map(_DIMOUSESTATE2* state) const override;
     void map(char* keyboardState) const override;
+
+private:
+    std::unique_ptr<B3L::InlinePatch> uiPatch = nullptr;
 };
