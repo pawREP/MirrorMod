3a3cb3a3654c549a3c9de5347e42233556f978a1
pawREP <pawREP@users.noreply.github.com>
2022-12-11 06:14:53 +0100

    Add proper key remapping for DSR
    Add code that handles keyboard state remapping correctly for DSR. Remapping DInput is not sufficient for this game

--- Mirror/src/DSR.cpp
+++ Mirror/src/DSR.cpp
@@ -1,6 +1,34 @@
 #include "DSR.h"
 #include "DInputUtil.h"
 #include "XInputUtil.h"
+#include <format>
+
+namespace {
+
+    // Original GetAsyncKeyState function
+    SHORT (*getAsyncKeyState)(int) = nullptr;
+
+    // Swap state for A and D key codes
+    SHORT remapKeystate(int vKey) {
+        if(vKey == 'A')
+            vKey = 'D';
+        else if(vKey == 'D')
+            vKey = 'A';
+
+        return getAsyncKeyState(vKey);
+    }
+
+} // namespace
+
+DSR::DSR([[maybe_unused]] const B3L::ImageView& image, [[maybe_unused]] const Configuration& config) {
+    // Unlike all other recent From Software games DSR doesn't utilize DINPUT to query the current keyboard state.
+    // GetAsyncKeyState is used instead which means we need an additional input hook here.
+
+    getAsyncKeyStateHook = std::make_unique<B3L::IatHook<SHORT (*)(int)>>("USER32.dll", "GetAsyncKeyState", &remapKeystate);
+    getAsyncKeyState = getAsyncKeyStateHook->originalTarget<SHORT(int)>();
+
+    getAsyncKeyStateHook->enable();
+}
 
 void DSR::map(_XINPUT_STATE* pState) const {
     XInputUtil::mirrorLeftStickX(pState);
--- Mirror/src/DSR.h
+++ Mirror/src/DSR.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "B3L/Hook.h"
 #include "Configuration.h"
 #include "DInputHook.h"
 #include "XInputHook.h"
@@ -6,9 +7,12 @@
 
 class DSR : public IXInputMap, public IDInputMap {
 public:
-    DSR([[maybe_unused]] const B3L::ImageView& image, [[maybe_unused]] const Configuration& config){};
+    DSR(const B3L::ImageView& image, const Configuration& config);
 
     void map(_XINPUT_STATE* pState) const override;
     void map(_DIMOUSESTATE2* state) const override;
     void map(char* keyboardState) const override;
+
+private:
+    std::unique_ptr<B3L::IatHook<SHORT (*)(int)>> getAsyncKeyStateHook = nullptr;
 };
